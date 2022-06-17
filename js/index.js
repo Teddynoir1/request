7746cccb8fd8a55b5584902ac76a04e08b021514
underscoreDev <godswill@zimmerman.team>
2022-06-17 15:56:03 +0100

    CRUD operations

--- js/index.js
+++ js/index.js
@@ -1,19 +1,28 @@
 const container = document.querySelector(".container");
-const form = document.querySelector("form");
-form.addEventListener("submit", (e) => e.preventDefault());
+const forms = document.querySelectorAll("form");
+forms.forEach((form) =>
+  form.addEventListener("submit", (e) => e.preventDefault())
+);
 
 let postsToBeRendered = [];
 
 const renderPosts = (incomingPosts) =>
   incomingPosts.forEach((post) => {
     const postCard = `
-        <div class="card" id=${post.id} >
+        <div class="card" id=${post.id}>
           <div class="box">
             <div class="content">
               <h2>${post.id}</h2>
               <h3>${post.title}</h3>
               <a href="/post.html?id=${post.id}">Read More</a>
-              <a>Update</a>
+
+              <a type="button"
+                  class="btn btn-primary"
+                  data-bs-toggle="modal"
+                  data-bs-target="#updatePost"
+                  onclick="getPostId(${post.id})">
+                  Update</a>
+
               <button onclick="deletePost(${post.id})" class="btn">
                 <i class="fa fa-trash"></i> Trash
               </button>
@@ -74,6 +83,54 @@ const createNewPost = async () => {
     const greg = JSON.parse(window.sessionStorage.getItem(`posts`));
 
     window.sessionStorage.setItem(`posts`, JSON.stringify([...greg, data]));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//
+//
+// UPDATE POST
+//
+//
+
+let postId;
+const getPostId = (id) => (postId = id);
+
+const updatePost = async () => {
+  const postBody = document.getElementById("update_post_body");
+  const postTitle = document.getElementById("update_post_title");
+
+  try {
+    const { data } = await axios({
+      method: "PUT",
+      url: `https://jsonplaceholder.typicode.com/posts/${postId}`,
+      data: {
+        id: postId,
+        title: postTitle.value,
+        body: postBody.value,
+        userId: 100,
+      },
+    });
+
+    // console.log(data);
+
+    const charles = JSON.parse(window.sessionStorage.getItem(`posts`));
+    const oldPost = charles.filter((post) => post.id === postId);
+
+    // oldPost.title = data.title;
+    // oldPost.body = data.body;
+    // console.log(...charles, charles.);
+
+    // window.sessionStorage.setItem(
+    //   "posts",
+    //   JSON.stringify([...charles, oldPost])
+    // );
+
+    // console.log(oldPost);
+
+    postBody.value = "";
+    postTitle.value = "";
   } catch (error) {
     console.log(error);
   }
